498bffe0e7a997c702345feb539059462546a9b3
tbb <enesg@protonmail.com>
2018-02-01 23:01:06 +0300

    visual improvements

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ function search() {
   var url = ("https://en.wikipedia.org/w/api.php?"
     + "action=query&format=json&list=search&utf8=&srsearch="
     + encodeURIComponent(input));
-  console.log("search");
   $("#spinner").removeClass("d-none");
   $.ajax({
     url,
@@ -19,14 +18,15 @@ function search() {
 function createEntries(json) {
   var data = json.query.search;
   var entries = $("#entries");
-  var template = entries.children();
+  var template = $("#template");
 
   $("#spinner").addClass("d-none");
+  entries.empty();
   for (var i = 0; i < data.length; i++) {
     var instance = template.clone().removeClass("d-none");
     instance.attr("href", "https://en.wikipedia.org/?curid=" + data[i].pageid);
     instance.find("h5").text(data[i].title);
-    instance.find("p").text(data[i].snippet + "...");
+    instance.find("p").html(data[i].snippet + "...");
     entries.append(instance);
   }
 }
